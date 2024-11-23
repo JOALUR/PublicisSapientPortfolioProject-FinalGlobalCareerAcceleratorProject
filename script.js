ff9b25c6b298c6737aa530507a6033939f404b22
Jose Uribe <116236557+JOALUR@users.noreply.github.com>
2024-11-23 00:05:30 +0000

    update for tasks 2 and 3, completed, wanted to update before moving onto task 4

--- script.js
+++ script.js
@@ -23,19 +23,38 @@ function createCard(place) {
   // Step 1: Create a new div element and store it in a variable
   let newDiv = document.createElement("div");
   // Step 2: Add the col class to the new div element
-  let newColumn = document.createElement("col");
-  newDiv.appendChild(newColumn);
-  // Step 3: Set the innerHTML of the div with a template string. It should resemble the structure shown in the readme. Interpolate the values for place.name, place.img, and place.location where needed. More info - https://wesbos.com/template-strings-html
+  newDiv.classList.add("col");
+  /* Step 3: Set the innerHTML of the div with a template string. 
+    It should resemble the structure shown in the readme. Interpolate 
+    the values for place.name, place.img, and place.location where needed. More info - https://wesbos.com/template-strings-html
+  */
+  newDiv.innerHTML = `
+    <div class = "card h-100" onclick = "centerPlaceOnMap(place.name)">
+      <img src="${place.img}" class="card-img-top h-100" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${place.name}</h5>
+        <p class="card-text">${place.location}</p>
+      </div>
+    </div>
+  `;
+
   // Step 4: Return the element
+  return newDiv;
 }
 
 // Task 3
 function populateRecommendationCards(filteredPlaces) {
   // Step 1: Store the DOM element with the id of "recommendations" in a variable
+  let recommendations = document.getElementById("recommendations");
   // Step 2: Clear the "recommendations" innerHTML
+  recommendations.innerHTML = "";
   // Step 3: Loop through the filteredPlaces array
-  // Step 4: Create a card for each place using the createCard function
-  // Step 5: Add/append each card to the recommendations DOM element
+  for (let i = 0; i < filteredPlaces.length; i++) {
+    // Step 4: Create a card for each place using the createCard function
+    let card = createCard(filteredPlaces[i]);
+    // Step 5: Add/append each card to the recommendations DOM element
+    recommendations.appendChild(card);
+  }
 }
 
 // Task 4
